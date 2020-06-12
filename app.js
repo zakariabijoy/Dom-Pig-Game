4a8a35d63c52a72a4790ac80ab81262e82127e10
zakariabijoy <zakaria.bijoy@live.com>
2020-06-13 03:07:49 +0600

    Implementing Our Hold Function and the DRY Principle

--- app.js
+++ app.js
@@ -40,7 +40,36 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         // Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // add current score to global score
+    scores[activePlayer] += roundScore;
+
+    // update the ui
+
+    document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+
+    // check if player won the game
+        if(scores[activePlayer] >= 100){
+            document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+            document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+
+
+        }else{
+            // next player
+            nextPlayer();
+        }
+    
+});
+
+function nextPlayer(){
+
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
 
         document.getElementById('current-0').textContent = '0';
@@ -53,6 +82,4 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         document.querySelector('.player-1-panel').classList.toggle('active');
 
         diceDom.style.display = 'none';
-
-    }
-});
+}
